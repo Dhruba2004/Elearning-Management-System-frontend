f467152dec8525b949521d2969f0e38508036999
Dhruba Dutta Banik <113227923+Dhruba2004@users.noreply.github.com>
2024-03-25 08:52:58 +0530

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children }) {
   return (
     <div>
       <Navbar />
-      <div className="min-h-screen">{children}
+      <div className="min-h-screen lg:w-[1534px] w-full overflow-x-hidden">{children}
       </div>
       <Footer />
     </div>
